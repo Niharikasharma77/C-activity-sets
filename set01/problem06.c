10cba2355aa486720a0efa8d959768b48e7d6c51
Niharikasharma77 <145001072+Niharikasharma77@users.noreply.github.com>
2024-02-12 13:46:14 +0530

    test

--- set01/problem06.c
+++ set01/problem06.c
@@ -3,45 +3,14 @@
 ***Function Declarations***
 
 ```c
-int input(int *a, int *b, int *c);
+void input(int *a, int *b, int *c);
 void compare(int a, int b, int c, int *largest);
 void output(int a, int b, int c, int largest);
 ```*/
-
-#include<stdio.h>
-int input(int *a,int *b, int *c);
-void compare(int a,int b,int c,int * largest);
+#include <stdio.h>
+void input(int *a,int *b,int *c);
+void compare(int a,int b,int c,int *largest);
 void output(int a,int b,int c,int largest);
-int input(int *a,int *b,int *c)
-{
-    int x;
-    printf("enter the value:");
-    scanf("%d",&x);
-    return x;
-}
-
-void compare(int a,int b,int c,int *largest)
-{
-    int largest;
-    if(a>=b && a>=c)
-    {
-        *largest = a;
-    }
-    if(b>=a && b>=c)
-    {
-        *largest =b;
-    }
-    else
-    {
-        *largest =c;
-    }
-}
-
-void output(int a,int b,int c,int largest)
-{
-    printf("the largest of %d,%d and %d is %d",a,b,c,largest);
-}
-
 int main()
 {
     int a,b,c,largest;
@@ -50,3 +19,34 @@ int main()
     output(a,b,c,largest);
     return 0;
 }
+
+void input(int *a,int *b,int *c)
+{
+    printf("enter the number a:");
+    scanf("%d", a);
+    printf("enter the number b:");
+    scanf("%d", b);
+    printf("enter the number c:");
+    scanf("%d", c);
+}
+
+void compare(int a,int b,int c,int *largest)
+{
+    if(a>=b && a>=c)
+    {
+        *largest = a;
+    }
+    if(b>=c)
+    {
+        *largest = b;
+    }
+    else{
+        *largest = c;
+    }
+}
+
+void output(int a,int b,int c,int largest)
+{
+    printf("the largest of %d,%d, and %d is %d",a,b,c,largest);
+}
+
