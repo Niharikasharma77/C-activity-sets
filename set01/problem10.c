7f0de08b3cf2bf7651c69638adceab09febeb004
Niharikasharma77 <145001072+Niharikasharma77@users.noreply.github.com>
2024-02-12 21:28:52 +0530

    problem10

--- set01/problem10.c
+++ set01/problem10.c
@@ -26,7 +26,7 @@ void input_two_strings(char *string1,char *string2)
 int stringcompare(char *string1,char *string2)
 {
     int i,result=0;
-    for(i=0;string1[i]!='\0' && string1[i]==string2[i];i++);
+    for(i=0;string1[i]!='\0' && string1[i]==string2[i];i++);//no need of putting string2[i]!='\0'
     result =string1[i] - string2[i];
     return result;
 }
