663ed9b7343368827f5ca0f9a794c37f51fed132
Niharikasharma77 <145001072+Niharikasharma77@users.noreply.github.com>
2024-02-16 15:10:26 +0530

    problem04debug

--- set02/problem05.c
+++ set02/problem05.c
@@ -13,19 +13,37 @@ int find_gcd(int a, int b);
 void output(int a, int b, int gcd);
 int main()
 {
-    
+    int a,b,gcd;
+    a = input();
+    b = input();
+    gcd = find_gcd(a,b);
+    output(a,b,gcd);
+    return 0;
 }
 
 int input()
 {
-    int a,b;
-    printf("enter the numbers:");
-    scanf("%d %d",&a, &b);
-    return(a ,b);
+    int a;
+    printf("enter the number :");
+    scanf("%d",&a);
+    return(a);
 }
 
 int find_gcd(int a,int b)
 {
     int gcd;
-    
+    for(int i=1;i<=a || i<=b ;i++)
+    {
+        if(a%i==0 && b%i ==0 )
+        {
+            gcd = i;
+        }
+    }
+    return(gcd);
 }
+
+void output(int a, int b, int gcd)
+{
+    printf("\nThe GCD of %d and %d is : %d\n",a,b,gcd);
+}
+
