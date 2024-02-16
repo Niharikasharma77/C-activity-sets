cfd2302de7884691a6b9c77e96e4797e57e99b19
Niharikasharma77 <145001072+Niharikasharma77@users.noreply.github.com>
2024-02-16 14:52:16 +0530

    test

--- set02/problem04.c
+++ set02/problem04.c
@@ -9,3 +9,55 @@ void output(int sum);
 ```*/
 
 #include <stdio.h>
+int input_array_size();
+void input_array(int n, int a[n]);
+int sum_composite_numbers(int n, int a[n]);
+void output(int sum);
+
+int main()
+{
+    int n = input_array_size();
+    int a[n];
+    input_array(n,a);
+    int sum = sum_composite_numbers(n,a);
+    output(sum);
+}
+
+int input_array_size()
+{
+    int n;
+    printf("enter the array size:");
+    scanf("%d",&n);
+    return n;
+}
+
+void input_array(int n, int a[n])
+{
+    printf("enter the number:");
+    for(int i =0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+}
+
+int sum_composite_numbers(int n,int a[n])
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=2;j<n;j++)
+        {
+            if(a[i]%j==0)
+            {
+                sum+=a[i];
+                break;
+            }
+        }
+    }
+    return sum;
+}
+
+void output(int sum)
+{
+    printf("the sum is %d\n",sum);
+}
---
+++ set02/problem05.c
@@ -0,0 +1,31 @@
+/*05.  Write a program to find GCD _(HCF)_ of two numbers.
+
+***Function Declarations***
+```c
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+```*/
+
+#include<stdio.h>
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+int main()
+{
+    
+}
+
+int input()
+{
+    int a,b;
+    printf("enter the numbers:");
+    scanf("%d %d",&a, &b);
+    return(a ,b);
+}
+
+int find_gcd(int a,int b)
+{
+    int gcd;
+    
+}
