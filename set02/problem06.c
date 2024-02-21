dc8d2fb4eeaacf5dcbf0118b8d845a8a1ad33c01
Niharikasharma77 <145001072+Niharikasharma77@users.noreply.github.com>
2024-02-21 21:15:25 +0530

    test

--- set02/problem06.c
+++ set02/problem06.c
@@ -9,20 +9,40 @@ void output(char *a, char *reverse_a);
 */
 
 #include<stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
 
-//wap to change the value of the variable in the main function
-
-int input(int *n);
 int main()
 {
-   
-    int a=4;
-    printf("%d",a);
-    input(&a);
-    printf("%d",a);
+    char a[100],b[100];
+    input_string(a);
+    str_reverse(a,b);
+    output(a,b);
+    return 0;
 }
 
-int input(int *n)
+void input_string(char *a)
 {
-    *n=7;
+    printf("enter the string:");
+    scanf("%s",a);
+}
+
+void str_reverse(char *str,char *str_reverse)
+{
+    char a[100],b[100];
+    for(int i=0;a[i]!='\0';i++)
+    {
+        for(int j=0;i>=0;j++,i--)
+        {
+            b[j]=a[i];
+            b[j]='\0';
+        }
+    }
+}
+
+void output(char *a,char *reverse_a)
+{
+    printf("the string is %s\n",a);
+    printf("reverse string is %s:",reverse_a);
 }
