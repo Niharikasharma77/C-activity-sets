6f3d7886c04b1aee4a669ea7c8eb357b83f6f16a
Niharikasharma77 <145001072+Niharikasharma77@users.noreply.github.com>
2024-02-26 21:54:00 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -37,7 +37,7 @@ void  input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, floa
 int  is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
     int result;
-    if(x1==y1 && x2==y2 && x3==y3 && x1==x2 && x1==x3 && x2==x3 && x1==y2 && x1==y3 && x2==y1 && x2==y3 && y1==y2 && y1==y3)
+    if(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
     {
         result==1;
     }
@@ -52,7 +52,7 @@ void  output(float x1, float y1, float x2, float y2,float x3, float y3, int resu
     {
         printf("it is triangle");
     }
-    else(result==0)
+    else if(result==0)
     {
         printf("not a triangle");
     }
--- set03/problem03.c
+++ set03/problem03.c
@@ -16,7 +16,7 @@ void output(int n, int result);
 int main()
 {
     int n,result;
-    n=input();
+    n=input_number();
     result=is_prime(n);
     output(n,result);
     return 0;
@@ -32,6 +32,26 @@ int input_number()
 
 int is_prime(int n)
 {
-    int result;
-    
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            return  0; // not prime
+            break;
+        }
+        
+    }
+    return 66;  
+}
+
+void output(int n, int result)
+{
+    if(result == 0)
+    {
+        printf("\n%d is a prime number.",n);
+    }
+    else
+    {
+        printf("\n%d is not a prime number.",n);
+    }
 }
